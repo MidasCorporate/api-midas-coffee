411a99f5c8044eb9efdb35bc60bbaff1536c139d
alessandro-silva <alessandro.rds03@gmail.com>
2020-06-27 23:42:03 -0400

    :coffee: add middleware auth and verify token

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,14 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
 
 routes.post('/sessions', SessionController.store);
 
